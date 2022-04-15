85c2e85ecf1e63638371de911f26b1404e2a15b0
Enrique Rodriguez <emrodriguez3e@gmail.com>
2022-04-14 22:12:38 -0700

    -Populated noteScroll widget, sizing issue hides list.

--- gui.py
+++ gui.py
@@ -18,9 +18,11 @@ def printhelp(i=0):
 sampleString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " \
                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " \
                "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip" \
-               " ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore " \
+               " ex ea commodo consequat. Duis aute irure dolor in reprehen derit in voluptate velit esse cillum dolore " \
                "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " \
                "officia deserunt mollit anim id est laborum. "
+
+
 
 root = Tk()
 root.title("Python Journal")
@@ -32,24 +34,27 @@ noteFrame = ttk.Frame(root)
 # Create the table view and lean everything to the left
 columns = ('name')
 noteView = ttk.Treeview(root, columns=columns, show='headings')
-noteView.pack(side=tkinter.LEFT, expand=True, fill='both')
 noteView.heading('name', text='')
 
 
+# ollowing code populates the tree view
+contacts = []
+for i in range(1,100):
+    contacts.append(str(i))
+
+for contact in contacts:
+    noteView.insert('', tkinter.END, values=contact)
+
+noteScroll = ttk.Scrollbar(noteView, orient=tkinter.VERTICAL, command=noteView.yview)
+noteScroll.pack(fill='y', side=tkinter.RIGHT)
+noteView.pack(side=tkinter.LEFT, expand=True, fill='both')
+
 # Create ability to scroll through list of notes
-
 text = Text(noteFrame, wrap='word')
-
 scroll = ttk.Scrollbar(noteFrame, orient='vertical')
 scroll.config(command=text.yview)
 scroll.pack(side=tkinter.RIGHT, fill='y')
-text.pack()
-
-
-
-notesScroll = ttk.Scrollbar(noteView, orient='vertical')
-notesScroll.pack(fill='y', side=tkinter.RIGHT)
-
+text.pack(fill='both', expand=True) #need to pack text widget last for the scroll attachment
 
 # Create editable text and place onto the right
 note = ScrolledText(root)
