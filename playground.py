c5b2d49ce01bc4aebe5d1c7de25231e1bb61881c
Enrique Rodriguez <emrodriguez3e@gmail.com>
2022-04-22 09:27:09 -0700

    -Testing functionality for file finding
    Signed-off-by: Enrique Rodriguez <emrodriguez3e@gmail.com>

--- playground.py
+++ playground.py
@@ -1,4 +1,26 @@
 import os
 noteFolder = []
 
-print(os.walk())
+currentPath = os.getcwd()
+print(os.listdir(currentPath))
+
+
+noteDirectory = os.path.join(currentPath, 'noteDirectory')
+
+
+
+
+if not os.path.isdir(noteDirectory):
+
+    os.mkdir(currentPath + "\\noteDirectory")
+
+    for i in range(1,11):
+        file = open("noteDirectory\\untitled"+str(i)+".txt", 'w')
+        file.close()
+        print("File has been created")
+else:
+    pass
+
+print(os.path.isdir(noteDirectory))
+
+os.remove(noteDirectory)
