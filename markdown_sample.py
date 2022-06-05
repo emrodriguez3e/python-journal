2b1cad12b74ca240a60ce52ce43cd0f55a2980c7
Enrique Rodriguez <emrodriguez3e@gmail.com>
2022-06-05 11:01:45 -0700

    -Update markdown sample
    Signed-off-by: Enrique Rodriguez <emrodriguez3e@gmail.com>

--- markdown_sample.py
+++ markdown_sample.py
@@ -3,30 +3,35 @@ from tkinter import *
 from tkinter import font as tkfont
 import re
 
-sampleText = '**Sample** Text. This **bolded**'
-boldpattern = re.findall("\*\*[a-zA-z]*\*\*", sampleText)
-
-for i in boldpattern:
-    print(i)
 
 root = Tk()
 
+bold_font = tkfont.Font(weight='bold')
 
 # Initially, not having both lines didn't auto update. Might be a better
 # Tkinter doesn't support compiled regular expressions??
 def func(event):
-    text.tag_add("bolded", '1.0', '1.9')
-    text.tag_config("bolded", font=bold_font)
+    start_position = '1.0'
+    count_var = StringVar()
+
+    while start_position != 'end':
+        pos = text.search("\*\*[a-zA-Z]+\*\*", start_position, nocase=1, stopindex='end', regexp=True, count=count_var)
+        start_position = '%s + %sc' % (pos, int(count_var.get())+1)
+        print(start_position)
+        text.tag_add('bold_font', pos, start_position) # Throw a 'bad text index "" here. Doesn't crash program tho'
+        text.tag_config('bold_font', font=bold_font)
 
 
-bold_font = tkfont.Font(weight='bold')
+    print("Position: " + pos + "\nCount: " + count_var.get())
+
+
+
 
 text = Text(root)
 
-text.insert(END, '**Text**')
-text.insert('1.0', ' Sample')
-text.tag_add("bolded", '1.0', '1.9')
-text.tag_config("bolded", font=bold_font)
+text.insert(END, "This is **awesome** stuff to **code**")
+# text.tag_add("bolded", '1.0', '1.9')
+# text.tag_config("bolded", font=bold_font)
 text.bind('<KeyRelease>', func)
 text.pack(expand=True, fill='both')
 
