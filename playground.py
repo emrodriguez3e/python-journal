9d6a2655f94b3789eaa4baff0c54c519ba723a02
Enrique Rodriguez <emrodriguez3e@gmail.com>
2022-07-13 14:09:25 -0700

    Update playground.py
    - Found code that might be able to work inserting images into text widget

--- playground.py
+++ playground.py
@@ -1,15 +1,28 @@
-import os
-import time
-
-created_list = []
-
-for i in os.listdir('noteDirectory'):
-    a = os.stat(os.path.join('noteDirectory',i))
-    # print(i, " :: ", str(time.ctime(a.st_atime)))
-    created_list.append([time.ctime(a.st_ctime), i])
-
-for i in sorted(created_list):
-    print(i)
+import tkinter as tk
+from tkinter import ttk
 
 
+"""
+This code was found from here, make sure to change file being used
 
+https://stackoverflow.com/questions/38180388/tkinter-how-to-insert-an-image-to-a-text-widget
+
+"""
+def add_image():
+    # This uses two different methods of image creation.
+    # Original post doesn't use subsampled version
+    img = tk.PhotoImage(file = r"img2.gif")  # TODO: Make sure to change this!
+    img_sub = img.subsample(15,15)
+    text.image_create(tk.END, image = img_sub) # Example 1
+    #text.window_create(tk.END, window = tk.Label(text, image = img_sub)) # Example 2
+
+root = tk.Tk()
+
+text = tk.Text(root)
+text.pack(padx = 20, pady = 20)
+
+tk.Button(root, text = "Insert", command = add_image).pack()
+
+
+
+root.mainloop()
