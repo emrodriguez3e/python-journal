4e0b1a00cba5d5d46c5a2d45280da39b4ccecded
Enrique Rodriguez <emrodriguez3e@gmail.com>
2022-07-18 09:57:50 -0700

    - Update playground.py. Seeing how to create scrollable view. Should work only with canvas really
    Signed-off-by: Enrique Rodriguez <emrodriguez3e@gmail.com>

--- playground.py
+++ playground.py
@@ -1,28 +1,44 @@
 import tkinter as tk
 from tkinter import ttk
 
-
 """
 This code was found from here, make sure to change file being used
 
 https://stackoverflow.com/questions/38180388/tkinter-how-to-insert-an-image-to-a-text-widget
 
 """
-def add_image():
-    # This uses two different methods of image creation.
-    # Original post doesn't use subsampled version
-    img = tk.PhotoImage(file = r"img2.gif")  # TODO: Make sure to change this!
-    img_sub = img.subsample(15,15)
-    text.image_create(tk.END, image = img_sub) # Example 1
-    #text.window_create(tk.END, window = tk.Label(text, image = img_sub)) # Example 2
+
+
+# def add_image():
+#     # This uses two different methods of image creation.
+#     # Original post doesn't use subsampled version
+#
+#     text.image_create(tk.END, image=img)  # Example 1
+#     text.window_create(tk.END, window=tk.Label(text, image=img))  # Example 2
+
+# text = tk.Text(root)
+# text.pack(padx=20, pady=20)
+
+# tk.Button(root, text="Insert", command=add_image).pack()
+#
+# img = tk.PhotoImage(file="img.png")
+# img_sub = img.subsample(2, 2)
 
 root = tk.Tk()
 
-text = tk.Text(root)
-text.pack(padx = 20, pady = 20)
+c = tk.Canvas(root, height=10)
+c.pack()
 
-tk.Button(root, text = "Insert", command = add_image).pack()
+for i in range(10):
+    ttk.Label(c, text='Sample Text').grid(row=i+1, column=1)
+
+s1 = ttk.Scale(c)
+
+s2 = tk.Scrollbar(c, orient='vertical', command=c.yview)
+
+s1.grid(row=0, column=2)
 
 
 
 root.mainloop()
+
