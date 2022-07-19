6da8ed4e07721451b008bc2593c68c1911c6bcef
Enrique Rodriguez <emrodriguez3e@gmail.com>
2022-07-19 11:33:14 -0700

    Update playground.py
    -Update playground to have better canvas scroll functionality

--- playground.py
+++ playground.py
@@ -23,22 +23,45 @@ https://stackoverflow.com/questions/38180388/tkinter-how-to-insert-an-image-to-a
 #
 # img = tk.PhotoImage(file="img.png")
 # img_sub = img.subsample(2, 2)
+########################################################################################################################
 
-root = tk.Tk()
+#Following code snippet doesn't work but is much closer to what I need I believe
+#https://stackoverflow.com/questions/7727804/tkinter-using-scrollbars-on-a-canvas
 
-c = tk.Canvas(root, height=10)
-c.pack()
+root = Tk()
 
-for i in range(10):
-    ttk.Label(c, text='Sample Text').grid(row=i+1, column=1)
-
-s1 = ttk.Scale(c)
-
-s2 = tk.Scrollbar(c, orient='vertical', command=c.yview)
-
-s1.grid(row=0, column=2)
+container = Frame(root)
+canvas = Canvas(container)
+#setting_panel = Frame(canvas)
 
 
+sample_text = Text(canvas, width=40, height=10, wrap='word')
+sample_text.pack(pady=5, expand=False)
 
+
+font_label = Label(canvas, text='Font Label')
+font_scale = ttk.Scale(canvas, orient=HORIZONTAL)
+
+font_label.pack(after=sample_text)
+font_scale.pack(after=sample_text)
+
+line_label = Label(canvas, text='Line Label')
+line_scale = ttk.Scale(canvas, orient=HORIZONTAL)
+
+line_label.pack(after=sample_text)
+line_scale.pack(after=sample_text)
+
+
+
+scroll = Scrollbar(container, orient='vertical', command=canvas.yview)
+scroll.pack(fill='y', side=RIGHT)
+
+#canvas.configure(yscrollcommand=scroll.set, scrollregion=canvas.bbox('all'))
+
+container.pack()
+canvas.pack(expand=True)
+#setting_panel.pack()
+
+root.geometry('200x200')
 root.mainloop()
 
