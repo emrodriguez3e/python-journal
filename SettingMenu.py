51ccded375c04b9de70ef52775eb26a9f9fcbb89
Enrique Rodriguez <emrodriguez3e@gmail.com>
2022-07-20 08:55:04 -0700

    - Update SettingMenu.py. Buttons are now included to forget or pack various widgets.
    Signed-off-by: Enrique Rodriguez <emrodriguez3e@gmail.com>

--- SettingMenu.py
+++ SettingMenu.py
@@ -6,13 +6,21 @@ class Settings(Frame):
     def __init__(self, parent=None):
         Frame.__init__(self, parent)
 
+        self.setting_buttons = Frame(self)
+        self.setting_buttons.pack(fill='x', expand=False)
+
+        self.typography_button = ttk.Button(self.setting_buttons, text='Typography')
+        self.typography_button.grid(row=0, column=0, padx=3, pady=3)
+        self.themes_button = ttk.Button(self.setting_buttons, text='Themes')
+        self.themes_button.grid(row=0, column=1, padx=3, pady=3)
+
         self.setting_text_frame = Frame(self)
 
-        self.text = Text(self, width=40, height=7, wrap='word')
-        self.text.pack(pady=5, expand=False)
+        self.text = Text(self, width=30, height=15, wrap='word')
+        self.text.pack()
 
         self.font_label = Label(self.setting_text_frame, text="Sample")
-        self.font_size_scale = ttk.Scale(self.setting_text_frame, orient=HORIZONTAL)
+        self.font_size_scale = ttk.Scale(self.setting_text_frame, orient=HORIZONTAL, from_=0, to=5)
         self.line_label = Label(self.setting_text_frame, text="Sample")
         self.line_space_scale = ttk.Scale(self.setting_text_frame, orient=HORIZONTAL)
 
@@ -21,7 +29,6 @@ class Settings(Frame):
         self.widgets()
 
     def widgets(self):
-
         self.font_size_scale.grid(row=0, column=0)
         self.font_label.grid(row=0, column=1)
         self.line_space_scale.grid(row=1, column=0)
