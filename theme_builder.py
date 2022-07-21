327302cd75cc10ea607a944f61908dc526938514
Enrique Rodriguez <emrodriguez3e@gmail.com>
2022-07-21 13:17:44 -0700

    theme update
    Update theme file

--- theme_builder.py
+++ theme_builder.py
@@ -2,8 +2,8 @@ from tkinter import *
 from tkinter import ttk
 
 class Theme(Frame):
-    Frame.__init__(self, parent)
-    
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
     
 if '__name__' == __main__:
     Theme().mainloop 
